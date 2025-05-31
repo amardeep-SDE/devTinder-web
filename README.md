bfdc8117714c3a770c6d91fe9b55838f76e2a23a
amardeep-SDE <amardeepdwivedi1494@gmail.com>
2025-05-31 13:10:56 +0530

    Navbar, footer, outlet, routing

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 
 - Created aVite + React application
 - Remove unnecessary code create a Welcome to My App app
+Install Tailwind CSS
+- Install Dasiy UI
+- Add Navbar component to App.jsx
+- Create a Navbar.jsx separate component file
+- Install react router dom
+- Create BrowserRouter > Routes > Route =/ Body > RouteChildren
+- Create an Outlet in your Body Component
+- Create a footer
+
+Body
+ - Navbar
+ - Route =/ feed
+ - Route =/login => Login
+ - Route =/connections => connections
+ - Router =/profile => Profile
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+
 const App = () => {
   return (
-      <>
-        <h1 className="text-3xl font-bold underline">Welcome to My App</h1>
-      </>
+    <>
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
